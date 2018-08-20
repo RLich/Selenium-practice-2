dcad92601655afd6dc5bc4e5c9d97944f177b122
Mortis7 <41342211+Mortis7@users.noreply.github.com>
2018-08-20 20:29:15 +0200

    admin_panel_login fix

--- Page_Objects/admin_panel_login.py
+++ Page_Objects/admin_panel_login.py
@@ -10,12 +10,12 @@ class Admin_Panel:
         wd = self.wd
         wd.get(Config.admin_panel)
 
-    def login(self):
+    def login(self, username, password):
         wd = self.wd
         username_box = wd.find_element_by_name
         username_box("username").clear()
-        username_box("username").send_keys(Config.username)
+        username_box("username").send_keys(username)
 
         password_box = wd.find_element_by_name
         password_box("password").clear()
-        password_box("password").send_keys(Config.password)
+        password_box("password").send_keys(password)
